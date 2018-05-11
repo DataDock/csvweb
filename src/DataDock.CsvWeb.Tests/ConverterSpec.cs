1dee3469efd7dfb2858823da1c3c9fb3a3e091e0
Kal <kal@techquila.com>
2018-05-11 12:53:29 +0100

    Initial code import

---
+++ src/DataDock.CsvWeb.Tests/ConverterSpec.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using DataDock.CsvWeb.Metadata;
+using DataDock.CsvWeb.Parsing;
+using DataDock.CsvWeb.Rdf;
+using FluentAssertions;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Parsing.Handlers;
+using Xunit;
+
+namespace DataDock.CsvWeb.Tests
+{
+    public class ConverterSpec
+    {
+        public static IEnumerable<object[]> ValidConversionData = new[]
+        {
+            new object[] {"data\\valid-table-1.json", "data\\countries.csv", "data\\valid-table-1-out.ttl" },
+            new object[] {"data\\valid-table-2.json", "data\\countries.csv", "data\\valid-table-2-out.ttl" },
+            new object[] {"data\\valid-table-3.json", "data\\countries.csv", "data\\valid-table-3-out.ttl" },
+            new object[] {"data\\valid-table-4.json", "data\\countries.csv", "data\\valid-table-4-out.ttl" },
+            new object[] {"data\\valid-table-5.json", "data\\countries.csv", "data\\valid-table-5-out.ttl" },
+            new object[] {"data\\valid-table-6.json", "data\\countries.csv", "data\\valid-table-6-out.ttl" },
+            new object[] {"data\\valid-table-7.json", "data\\countries.csv", "data\\valid-table-7-out.ttl" },
+            new object[] {"data\\valid-table-suppressed-columns.json", "data\\countries.csv", "data\\valid-table-suppressed-columns-out.ttl" },
+            new object[] {"data\\empty_column.metadata.json", "data\\empty_column.csv", "data\\empty_column.out.ttl"}, 
+            new object[] {"data\\escaping.metadata.json", "data\\escaping.csv", "data\\escaping.out.ttl"}
+        };
+
+        [Theory]
+        [MemberData(nameof(ValidConversionData))]
+        public void TestValidConversions(string tableMetadataPath, string csvFilePath, string expectedOutputGraphPath)
+        {
+            var metadataParser = new JsonMetadataParser(null);
+            object metadataObject;
+            using (var metadataReader = File.OpenText(tableMetadataPath))
+            {
+                metadataObject = metadataParser.Parse(metadataReader);
+            }
+            var tableMeta = metadataObject as Table;
+            tableMeta.Should().NotBeNull(because:"The metadata file should parse as table metadata");
+            var outputGraph = new Graph();
+            var graphHandler = new GraphHandler(outputGraph);
+            var converter = new Converter(tableMeta, graphHandler);
+            using (var csvReader = File.OpenText(csvFilePath))
+            {
+                converter.Convert(csvReader);
+            }
+            converter.Errors.Count.Should().Be(0, "Expected 0 errors. Got {0}. Error listing is:\n{1}", converter.Errors.Count, string.Join("\n", converter.Errors));
+            var turtleParser = new TurtleParser(TurtleSyntax.W3C);
+            var expectGraph = new Graph();
+            turtleParser.Load(expectGraph, expectedOutputGraphPath);
+
+            var diff = new GraphDiff();
+            var diffReport = diff.Difference(expectGraph, outputGraph);
+            diffReport.AreEqual.Should().BeTrue();
+        }
+    }
+}
---
+++ src/DataDock.CsvWeb.Tests/JsonMetadataParserSpec.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using DataDock.CsvWeb.Metadata;
+using DataDock.CsvWeb.Parsing;
+using FluentAssertions;
+using Xunit;
+
+namespace DataDock.CsvWeb.Tests
+{
+    public class JsonMetadataParserSpec
+    {
+        public static IEnumerable<object[]> ValidParserTests
+        {
+            get
+            {
+                var ret = new List<object[]>
+                {
+                    new object[]
+                    {
+                        "data\\valid-table-1.json", null, new Table {Url = new Uri("http://example.org/countries.csv")}
+                    }
+                };
+
+
+                var t = new Table {Url = new Uri("http://example.org/countries.csv")};
+                t.TableSchema = new Schema(t) {Columns = new List<ColumnDescription>()};
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema) {Name = "countryCode"});
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "latitude",
+                    Datatype = new DatatypeDescription {Base = "decimal"}
+                });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "longitude",
+                    Datatype = new DatatypeDescription {Base = "decimal"}
+                });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "name"
+                });
+
+                ret.Add(new object[]
+                {
+                    "data\\valid-table-2.json", null, t
+                });
+
+
+                t = new Table { Url = new Uri("http://example.org/countries.csv") };
+                t.TableSchema = new Schema(t)
+                {
+                    AboutUrl = new UriTemplate("http://example.org/countries.csv/{_row}"),
+                    Columns = new List<ColumnDescription>()
+                };
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema) { Name = "countryCode" });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "latitude",
+                    Datatype = new DatatypeDescription { Base = "decimal" }
+                });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "longitude",
+                    Datatype = new DatatypeDescription { Base = "decimal" }
+                });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "name"
+                });
+
+                ret.Add(new object[] {"data\\valid-table-3.json", null, t});
+
+                t = new Table { Url = new Uri("http://example.org/countries.csv") };
+                t.TableSchema = new Schema(t)
+                {
+                    AboutUrl = new UriTemplate("http://example.org/countries/{countryCode}"),
+                    Columns = new List<ColumnDescription>()
+                };
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "countryCode",
+                    PropertyUrl = new UriTemplate("http://example.org/countries.csv/def/countryCode")
+                });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "latitude",
+                    Datatype = new DatatypeDescription { Base = "decimal" }
+                });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "longitude",
+                    Datatype = new DatatypeDescription { Base = "decimal" }
+                });
+                t.TableSchema.Columns.Add(new ColumnDescription(t.TableSchema)
+                {
+                    Name = "name"
+                });
+
+                ret.Add(new object[] {"data\\valid-table-6.json", null, t});
+                return ret;
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidParserTests))]
+        public void TestParseOfValidMetadataJson(string jsonPath, Uri baseUri, object expectedResult)
+        {
+            var metadataParser = new JsonMetadataParser(baseUri);
+            using (var fileStream = File.OpenText(jsonPath))
+            {
+                var parsed = metadataParser.Parse(fileStream);
+                parsed.Should().BeEquivalentTo(expectedResult, options=> options.Excluding(o=>o.SelectedMemberPath.EndsWith(".Parent")));
+            }
+        }
+    }
+}
